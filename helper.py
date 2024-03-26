cc32430e33affb52480e4501d6f93b7e5887ea3a
Dima Nafanets <dima.nafanets@gmail.com>
2024-03-26 19:41:15 +0200

    Create flask app-adopt a pet

---
+++ helper.py
@@ -0,0 +1,36 @@
+pets = {
+    'dogs': [
+        {
+            'name': 'Spot',
+            'age': 2,
+            'breed': 'Dalmatian',
+            'description': 'Spot is an energetic puppy who seeks fun and adventure!',
+            'url': './resources_img/Dalmatian.jpeg'
+        },
+        {
+            'name': 'Shadow',
+            'age': 4,
+            'breed': 'Border Collie',
+            'description': 'Eager and curious, Shadow enjoys company and can always be found tagging along at your heels!',
+            'url': './resources_img/Border_Collie.jpeg'
+        }
+    ],
+    'cats': [
+        {
+            'name': 'Snowflake',
+            'age': 1,
+            'breed': 'Tabby',
+            'description': 'Snowflake is a playful kitten who loves roaming the house and exploring.',
+            'url': './resources_img/Tabby.jpeg'
+        }
+    ],
+    'rabbits': [
+        {
+            'name': 'Easter',
+            'age': 4,
+            'breed': 'Mini Rex',
+            'description': 'Easter is a sweet, gentle rabbit who likes spending most of the day sleeping.',
+            'url': './resources_img/Mini_Rex.jpeg'
+        }
+    ]
+}
